cf91fbfb74db3b0ae90e36bde8fd6bb6b4c43e35
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-01-06 19:43:10 +0530

    Created DB Models

---
+++ server/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("post", postSchema);
+module.exports = Post;
